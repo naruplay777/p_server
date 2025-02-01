440fc9ab81c094dae10e6cfb2d11125145260c2d
naruplay777 <narugonzalez13@gmail.com>
2025-02-01 17:06:19 -0400

    pi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(463, 164);
+            this.button1.Location = new System.Drawing.Point(456, 154);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(109, 70);
             this.button1.TabIndex = 0;
-            this.button1.Text = "pipi";
+            this.button1.Text = "pi";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
