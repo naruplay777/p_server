fed39383db910f1045d336da2c0c21072a13d3c9
naruplay777 <narugonzalez13@gmail.com>
2025-02-08 04:19:06 -0400

    Iniciamos :D

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.listClients = new System.Windows.Forms.ListBox();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartServer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // listClients
             // 
-            this.button1.Location = new System.Drawing.Point(456, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 70);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "pi";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.listClients.FormattingEnabled = true;
+            this.listClients.Location = new System.Drawing.Point(11, 74);
+            this.listClients.Name = "listClients";
+            this.listClients.Size = new System.Drawing.Size(366, 56);
+            this.listClients.TabIndex = 0;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(11, 136);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(366, 211);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(11, 33);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(366, 35);
+            this.btnStartServer.TabIndex = 3;
+            this.btnStartServer.Text = "button1";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(841, 360);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.listClients);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listClients;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartServer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,17 +7,189 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
 
 namespace p_server
 {
     public partial class Form1 : Form
     {
+        private TcpListener server;
+        private Thread serverThread;
+        private bool isServerRunning = false;
+        private List<TcpClient> connectedClients = new List<TcpClient>();
+        private Queue<string> requestQueue = new Queue<string>();
+        private object lockObject = new object();
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void StartServer()
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(ip, 5000);
+                server.Start();
+
+                while (isServerRunning)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    connectedClients.Add(client);
+                    UpdateClientList(client.Client.RemoteEndPoint.ToString());
+
+                    Thread clientThread = new Thread(() => HandleClient(client));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateLog($"Error: {ex.Message}");
+            }
+        }
+
+        private void StopServer()
+        {
+            isServerRunning = false;
+            server.Stop();
+            foreach (TcpClient client in connectedClients)
+            {
+                client.Close();
+            }
+            connectedClients.Clear();
+            UpdateLog("Servidor detenido.");
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            try
+            {
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    // Agregar solicitud a la cola
+                    lock (lockObject)
+                    {
+                        requestQueue.Enqueue(request);
+                        UpdateLog($"Solicitud recibida de {client.Client.RemoteEndPoint}");
+                    }
+
+                    // Simular procesamiento y enviar respuesta
+                    string response = "SOLICITUD_PROCESADA";
+                    byte[] responseData = Encoding.ASCII.GetBytes(response);
+                    stream.Write(responseData, 0, responseData.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateLog($"Cliente desconectado: {ex.Message}");
+                connectedClients.Remove(client);
+                client.Close();
+            }
+        }
+
+        private void timerQuantum_Tick(object sender, EventArgs e)
+        {
+            if (requestQueue.Count > 0)
+            {
+                lock (lockObject)
+                {
+                    string request = requestQueue.Dequeue();
+                    ProcessRequest(request);
+                }
+            }
+        }
+
+        private void ProcessRequest(string request)
+        {
+            // Ejemplo: Analizar el archivo de texto recibido
+            string[] lines = request.Split('\n');
+            string resourceType = lines[0]; // Primera línea: tipo de recurso
+            int linesToPrint = int.Parse(lines[1]); // Segunda línea: cantidad de líneas
+
+            // Simular impresión (mostrar en RichTextBox)
+            Invoke((MethodInvoker)delegate {
+                txtLog.AppendText($"Imprimiendo {linesToPrint} líneas...\n");
+            });
+
+            // Registrar en archivo log
+            LogToFile($"Operación procesada: {linesToPrint} líneas");
+        }
+
+
+
+        private void LogToFile(string message)
+        {
+            string logPath = "servidor_log.txt";
+            string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}\n";
+
+            File.AppendAllText(logPath, logEntry);
+            UpdateLog($"Registro guardado: {message}");
+        }
+
+        private void UpdateLog(string message)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateLog), message);
+            }
+            else
+            {
+                txtLog.AppendText($"{DateTime.Now:HH:mm:ss} - {message}\n");
+            }
+        }
+
+
+        private void UpdateClientList(string clientInfo)
+        {
+            if (listClients.InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateClientList), clientInfo);
+            }
+            else
+            {
+                listClients.Items.Add(clientInfo);
+            }
+        }
+
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            if (!isServerRunning)
+            {
+                serverThread = new Thread(new ThreadStart(StartServer));
+                serverThread.IsBackground = true;
+                serverThread.Start();
+                btnStartServer.Text = "Detener Servidor";
+                label1.Text = "Detener Servidor";
+                isServerRunning = true;
+                UpdateLog("Servidor iniciado en 127.0.0.1:5000");
+            }
+            else
+            {
+                StopServer();
+                btnStartServer.Text = "Iniciar Servidor";
+                label1.Text = "Iniciar Servidor";
+                isServerRunning = false;
+            }
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
