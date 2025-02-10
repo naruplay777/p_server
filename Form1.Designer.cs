840c16330e01a20e527fbb5b44d256281eea891c
elizabeth15zabeth@gmail.com <elizabeth15zabeth@gmail.com>
2025-02-09 23:52:36 -0400

    prueba2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnStartServer = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listClients
@@ -80,11 +81,21 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "hola paola :D";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "negro:]";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(841, 360);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnStartServer);
             this.Controls.Add(this.label1);
@@ -105,6 +116,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnStartServer;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
