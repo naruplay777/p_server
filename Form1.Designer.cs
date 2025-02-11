35cb06ef7b5dc1a6c24458792c5a6f8151c0dc97
nathegold <genesissarrieche@gmail.com>
2025-02-11 12:04:16 -0400

    Cambio de colores y fuentes en la vista del servidor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,32 +47,37 @@
             // 
             // listClients
             // 
-            this.listClients.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.listClients.BackColor = System.Drawing.SystemColors.Control;
             this.listClients.FormattingEnabled = true;
-            this.listClients.Location = new System.Drawing.Point(12, 51);
+            this.listClients.ItemHeight = 16;
+            this.listClients.Location = new System.Drawing.Point(16, 63);
+            this.listClients.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listClients.Name = "listClients";
-            this.listClients.Size = new System.Drawing.Size(333, 56);
+            this.listClients.Size = new System.Drawing.Size(443, 68);
             this.listClients.TabIndex = 0;
             this.listClients.SelectedIndexChanged += new System.EventHandler(this.listClients_SelectedIndexChanged);
             // 
             // txtLog
             // 
-            this.txtLog.BackColor = System.Drawing.Color.PaleGoldenrod;
-            this.txtLog.Location = new System.Drawing.Point(17, 208);
+            this.txtLog.BackColor = System.Drawing.SystemColors.Control;
+            this.txtLog.Location = new System.Drawing.Point(17, 258);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
-            this.txtLog.Size = new System.Drawing.Size(349, 186);
+            this.txtLog.Size = new System.Drawing.Size(442, 228);
             this.txtLog.TabIndex = 1;
             this.txtLog.Text = "";
             // 
             // btnStartServer
             // 
-            this.btnStartServer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(46)))));
-            this.btnStartServer.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartServer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(87)))), ((int)(((byte)(70)))));
+            this.btnStartServer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStartServer.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStartServer.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.btnStartServer.Location = new System.Drawing.Point(9, 123);
+            this.btnStartServer.Location = new System.Drawing.Point(17, 151);
+            this.btnStartServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStartServer.Name = "btnStartServer";
-            this.btnStartServer.Size = new System.Drawing.Size(333, 35);
+            this.btnStartServer.Size = new System.Drawing.Size(442, 43);
             this.btnStartServer.TabIndex = 3;
             this.btnStartServer.Text = "Iniciar Servidor";
             this.btnStartServer.UseVisualStyleBackColor = false;
@@ -80,58 +85,63 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(57)))), ((int)(((byte)(27)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(57)))), ((int)(((byte)(39)))));
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(841, 56);
+            this.panel1.Size = new System.Drawing.Size(1121, 69);
             this.panel1.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Red;
-            this.label5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label5.Location = new System.Drawing.Point(127, 24);
+            this.label5.Location = new System.Drawing.Point(225, 27);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 15);
+            this.label5.Size = new System.Drawing.Size(79, 21);
             this.label5.TabIndex = 12;
             this.label5.Text = "Inactivo";
             // 
             // panel4
             // 
-            this.panel4.Location = new System.Drawing.Point(545, 56);
+            this.panel4.Location = new System.Drawing.Point(727, 69);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(28, 281);
+            this.panel4.Size = new System.Drawing.Size(37, 346);
             this.panel4.TabIndex = 11;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(60)))), ((int)(((byte)(27)))));
-            this.panel3.Location = new System.Drawing.Point(532, 56);
+            this.panel3.Location = new System.Drawing.Point(709, 69);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(40, 406);
+            this.panel3.Size = new System.Drawing.Size(53, 500);
             this.panel3.TabIndex = 9;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(60)))), ((int)(((byte)(27)))));
-            this.label3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label3.Location = new System.Drawing.Point(12, 16);
+            this.label3.Location = new System.Drawing.Point(16, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 29);
+            this.label3.Size = new System.Drawing.Size(175, 34);
             this.label3.TabIndex = 0;
-            this.label3.Text = "Servidor";
+            this.label3.Text = "SERVIDOR";
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(110)))), ((int)(((byte)(60)))));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(184)))), ((int)(((byte)(176)))));
             this.panel2.Controls.Add(this.panel5);
             this.panel2.Controls.Add(this.btnOpenLogs);
             this.panel2.Controls.Add(this.label4);
@@ -139,26 +149,32 @@
             this.panel2.Controls.Add(this.btnStartServer);
             this.panel2.Controls.Add(this.txtLog);
             this.panel2.Controls.Add(this.listClients);
-            this.panel2.Location = new System.Drawing.Point(0, 55);
+            this.panel2.Location = new System.Drawing.Point(0, 68);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(604, 406);
+            this.panel2.Size = new System.Drawing.Size(805, 500);
             this.panel2.TabIndex = 7;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // panel5
             // 
-            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(60)))), ((int)(((byte)(27)))));
-            this.panel5.Location = new System.Drawing.Point(545, -4);
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(57)))), ((int)(((byte)(39)))));
+            this.panel5.Location = new System.Drawing.Point(727, -5);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(41, 410);
+            this.panel5.Size = new System.Drawing.Size(55, 505);
             this.panel5.TabIndex = 11;
             // 
             // btnOpenLogs
             // 
-            this.btnOpenLogs.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.btnOpenLogs.Location = new System.Drawing.Point(395, 56);
+            this.btnOpenLogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(87)))), ((int)(((byte)(70)))));
+            this.btnOpenLogs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOpenLogs.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenLogs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOpenLogs.Location = new System.Drawing.Point(527, 69);
+            this.btnOpenLogs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOpenLogs.Name = "btnOpenLogs";
-            this.btnOpenLogs.Size = new System.Drawing.Size(112, 43);
+            this.btnOpenLogs.Size = new System.Drawing.Size(149, 53);
             this.btnOpenLogs.TabIndex = 10;
             this.btnOpenLogs.Text = "Abrir Logs";
             this.btnOpenLogs.UseVisualStyleBackColor = false;
@@ -167,33 +183,37 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(19, 178);
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(18, 222);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 16);
+            this.label4.Size = new System.Drawing.Size(256, 23);
             this.label4.TabIndex = 5;
-            this.label4.Text = "InfoServidor :";
+            this.label4.Text = "Información del Servidor ";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(14, 23);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(13, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.Size = new System.Drawing.Size(123, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "Conectados";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 461);
+            this.ClientSize = new System.Drawing.Size(781, 567);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
